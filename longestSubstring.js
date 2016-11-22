b559705a1335e8dea9ccfbac54cc440a3d1c3650
Vahid Mazde <vahid.mazdeh@fourwindsinteractive.com>
2016-11-21 20:31:45 -0700

    infinite loop

--- longestSubstring.js
+++ longestSubstring.js
@@ -4,6 +4,29 @@
 */
 
 var lengthOfLongestSubstring = function (s) {
-  console.log('s: ', s);
-  return res.length;
+  let i = 0,
+      j = 0,
+      subString = "",
+      counters = [0];
+    
+    while (j < s.length) {
+      console.log('subs: ', j, s[j]);
+        while (!subString.includes(s[i]) && i < s.length) {
+          console.log('substring: ', subString);
+          subString += s[i];
+          i++;
+        }
+        counters.push(subString.length);
+        j = subString.indexOf(s[i]) + 1;
+        subString = "";
+    }
+        
+    function getMaxofArray(a) {
+       return Math.max.apply(null, a);
+    }
+
+    return getMaxofArray(counters);
 }
+
+let result = lengthOfLongestSubstring('dvdf');
+console.log('res: ', result);
